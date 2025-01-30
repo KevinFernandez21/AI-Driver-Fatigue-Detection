ceb33b89e99f11b175376573b5b1c69b65f92411
KevinFernandez21 <kdfernan@espol.edu.ec>
2025-01-30 08:18:33 -0500

    server

--- backend/app/main.py
+++ backend/app/main.py
@@ -100,6 +100,6 @@ async def predict(file: UploadFile = File(...)):
             content={"message": f"Error interno del servidor: {str(e)}"}
         )
     
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Usa el puerto 8080 por defecto
-    app.run(host="0.0.0.0", port=port)
+#if __name__ == "__main__":
+    #port = int(os.environ.get("PORT", 8080))  # Usa el puerto 8080 por defecto
+    #app.run(host="0.0.0.0", port=port)
